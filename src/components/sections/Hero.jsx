8e16e8d48752dd1fd0c6106851feb7771a332dd7
kaliankhe <137717528+kaliankhe@users.noreply.github.com>
2024-09-16 22:31:37 +0300

    add image for icon

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Bio } from "../../data/constants";
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/HeroImage.jpg";
+import HeroImg from "../../images/HeroImage.png";
 import HeroBgAnimation from "../HeroBgAnimation";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
@@ -256,7 +256,7 @@ const Hero = () => {
             <HeroRightContainer>
               <motion.div {...headContentAnimation}>
                 <Tilt>
-                  <Img src={HeroImg} alt="Rishav Chanda" />
+                  <Img src={HeroImg} alt="Aslam Mahi" />
                 </Tilt>
               </motion.div>
             </HeroRightContainer>
