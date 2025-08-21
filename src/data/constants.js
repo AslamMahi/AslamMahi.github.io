84dd189667cec1db578db58e372a4d2b7703e049
Aslam Mahimkar <aslam.mahimkar@gmail.com>
2025-08-21 09:47:48 +0300

    Update resume and other files

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "As a Red Teamer and Penetration Tester, I focus on finding vulnerabilities and enhancing security measures. My passion for cybersecurity drives me to continuously learn and stay ahead of emerging threats. With experience as a SOC analyst, I bring a strong understanding of threat detection and response to my work.",
   github: "https://github.com/AslamMahi",
   resume:
-    "/AslamMahimkar-Resumev4-1.pdf",
+    "/AslamMahimkar-Resumev4-2.pdf",
   linkedin: "https://www.linkedin.com/in/aslam-mahimkar-642a74139",
   /*twitter: "https://twitter.com/RishavChanda",*/
 
